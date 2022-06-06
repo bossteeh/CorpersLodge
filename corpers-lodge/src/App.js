9e8de94088794fcc14e30ea0ba510519b89cafe4
bossteeh <akinrinolaelijah@gmail.com>
2022-06-06 07:36:00 -0600

    section one completed

--- corpers-lodge/src/App.js
+++ corpers-lodge/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import SectionOne from './components/SectionOne';
 
 function App() {
   return (
     <div>
       <Header />
+      <SectionOne />
     </div>
   );
 }
--- corpers-lodge/src/components/SectionOne.jsx
+++ corpers-lodge/src/components/SectionOne.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import image from './Images/Group312.png';
 
 const SectionOne = () => {
-    return ( 
-        <section>
-            
-        </section>
-     );
-}
- 
+  return (
+    <section className="firstSection container">
+      <div className="text">
+        <h2>Find your perfect</h2>
+        <h2>
+          home <span>Anywhere</span>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis
+          orci hendrerit feugiat odio a amet. Vestibulum in eget massa sit id.
+          Sem justo, in massa nunc faucibus purus. Aliquam nulla nulla sagittis
+          eget neque dictum.
+        </p>
+        <div className='btns'>
+          <button>Get Started</button>
+          <button className='learnbtn'>Learn More</button>
+        </div>
+      </div>
+      <div className="building">
+        <img src={image} alt="" srcset="" />
+      </div>
+    </section>
+  );
+};
+
 export default SectionOne;
