53debdf945a892bfe2a233fbd8a5881391464196
bossteeh <akinrinolaelijah@gmail.com>
2022-06-07 05:21:27 -0600

    header responsiveness completed

--- corpers-lodge/src/components/Header.jsx
+++ corpers-lodge/src/components/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Style/styles.css'
-import { Fabars, Fatimes } from "react-icons/fa"
+import { FaBars, FaTimes } from "react-icons/fa"
     
 const Header = () => {
+
+    const [isMobile, setMobile] = useState(true);
+
+    const handleMobile = () => {
+        setMobile((prev) => !prev);
+    }
 
     return ( 
         <header>
             <nav className='navbar container'>
                 <div className='logotext'>Corpers <span>Lodge</span></div>
-                <ul className='links'>
+                <ul className={`${isMobile ? 'links' : 'ul'}`}>
                     <li className='home'>Home</li>
                     <li className="about">Listing</li>
                     <li className='about'>About us</li>
@@ -16,7 +22,9 @@ const Header = () => {
                     <li className="contact">Login</li>
                     <button><li className="contact">Sign Up</li></button>
                 </ul> 
-                <div></div>
+                <span className='switcher' onClick={handleMobile}>
+                {isMobile ? <FaBars /> : <FaTimes />}
+            </span>
             </nav>
         </header>
      );
